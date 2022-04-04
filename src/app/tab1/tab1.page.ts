ad6531f7a53f3a76cb98e8be1196a4cf20a2e78a
Roman Kostyuchenko <elromankos@gmail.com>
2022-04-04 16:12:42 +0200

    1st commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,27 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+    let numero = 10;
+    //debugger;
+
+    numero = 20;
+
+    numero = numero + 10;
+
+    numero = 0;
+
+      console.log(numero);
+  }
+
+}
